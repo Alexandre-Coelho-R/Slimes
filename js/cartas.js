b32ffb470c2e8d4ca1fab4300953e7135ffe711a
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-07-17 18:21:40 -0300

    Melhoria do catálogo de cartas e sistema de ampliar a carta ao clicá-la

--- js/cartas.js
+++ js/cartas.js
@@ -11,8 +11,6 @@ const btFerramentas = document.getElementById("ferramentas");
 const cartaAmpliada = document.getElementById("carta-ampliada");
 
 const cartas = [{nome:"Slime necromante",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime mágico",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime primitivo",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime solar",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime de grama",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime MUITO MUITO MUITO grande",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime Alquimista",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime comum",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime buraco negro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime enfermeiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime elétrico",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime aquático",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime espadachim",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Fragilizador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime clérigo",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Atomislime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime de magma",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Dj slime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime de cola",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime Ferreiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime mago",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Draconislime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime pescador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime escavador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime cozinheiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slime Viking",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Aspirador de slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Soprador de slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"kit caça slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"colheita de gosma",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Slimonomicon",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Poção de escudo",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Poção de cura",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"relação interespecífica",categoria:"Ferramenta",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Ritual de desespero",categoria:"Ferramenta",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Dança das cadeiras",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Imitar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Pesquisar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Reciclar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"EU ESCOLHO VOCÊ",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Impressora",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"},{nome:"Limpar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.png"}];
-// const cartas = [{nome:"Slime necromante",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime mágico",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime primitivo",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime solar",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime de grama",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime MUITO MUITO MUITO grande",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime Alquimista",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime comum",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime buraco negro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime enfermeiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime elétrico",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime aquático",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime espadachim",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Fragilizador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime clérigo",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Atomislime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime de magma",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Dj slime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime de cola",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime Ferreiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime mago",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Draconislime",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime pescador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime escavador",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime cozinheiro",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slime Viking",categoria:"Slime",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Aspirador de slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Soprador de slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"kit caça slime",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"colheita de gosma",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Slimonomicon",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Poção de escudo",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Poção de cura",categoria:"Item",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"relação interespecífica",categoria:"Ferramenta",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Ritual de desespero",categoria:"Ferramenta",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Dança das cadeiras",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Imitar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Pesquisar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Reciclar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"EU ESCOLHO VOCÊ",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Impressora",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"},{nome:"Limpar",categoria:"Ação",imagem:"imagens/cartas/slime-provisorio.aaa"}];
-
 
 //Função de mostrar cartas
 
@@ -45,14 +43,45 @@ function desativarCores(){
 //Função de ampliar carta quando clica nela
 
 function ampliarCarta(carta){
+    if (window.innerWidth < 500) return;
+
     cartaAmpliada.innerHTML = "";
 
-    let cartaCriada = document.createElement("img");
+    const cartaCriada = document.createElement("img");
     cartaCriada.src = carta.imagem;
     cartaCriada.alt = carta.nome;
 
     cartaAmpliada.appendChild(cartaCriada);
     cartaAmpliada.classList.add("aberta");
+
+    //Movimento ao mexer o mouse
+
+    cartaCriada.addEventListener("mousemove", (evento) => {
+        const coordenadas = cartaCriada.getBoundingClientRect();
+
+        const mouseX = evento.clientX;
+        const mouseY = evento.clientY;
+
+        const centroY = coordenadas.top + coordenadas.height / 2;
+        const centroX = coordenadas.left + coordenadas.width / 2;
+
+        const relativoY = (2 * (mouseY - centroY) / coordenadas.height) * -15;
+        const relativoX = (2 * (mouseX - centroX) / coordenadas.width) * 15;
+
+        cartaCriada.style.transform = `
+            perspective(800px)
+            rotateX(${relativoY}deg)
+            rotateY(${relativoX}deg)
+        `;
+    });
+
+    cartaCriada.addEventListener("mouseout", (evento) => {
+        cartaCriada.style.transform = `
+            perspective(800px)
+            rotateX(0deg)
+            rotateY(0deg)
+        `
+    });
 }
 
 //Inicialização
