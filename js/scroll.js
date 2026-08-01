5015ce41cd98b57b5adacb4b4a67eb58ba7e3e40
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-01 14:43:24 -0300

    Remoção do botão de voltar ao topo se a tela não tiver scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,16 @@
 const botao = document.getElementById("subir");
 
 function verificarScroll() {
-    if (document.documentElement.scrollHeight < window.innerHeight) {
-        subir.style.display = "none";
+    if (document.documentElement.scrollHeight <= window.innerHeight + 3) {
+        botao.style.display = "none";
+    } else {
+        botao.style.display = "";
     }
 }
 
 botao.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: "smooth"});
 });
+
+window.addEventListener("load", verificarScroll);
+window.addEventListener("resize", verificarScroll);
