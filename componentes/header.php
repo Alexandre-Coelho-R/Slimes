406d2288cab6b5bf93804028c32a57bc3614c1db
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-05 22:29:40 -0300

    Início do desenvolvimento do novo header

--- componentes/header.php
+++ componentes/header.php
@@ -6,10 +6,15 @@ $pagina_ativada = $pagina_ativada ?? "";
     <a href="index.php" id="logo">
         <img src="imagens/logo_provisoria.png" alt="Logo do projeto Pocket Slimes">
     </a>
+
     <nav id="menus">
         <a href="index.php" class="<?php echo $pagina_ativada === "index.php" ? "ativo" : ""?>">
             <i class="fa-solid fa-house"></i>
             <p>Início</p>
+        </a>
+        <a href="produtos.php" class="<?php echo $pagina_ativada === "produtos.php" ? "ativo" : ""?>">
+            <i class="fa-solid fa-box-open"></i>
+            <p>Produtos</p>
         </a>
         <a href="tutoriais.php" class="<?php echo $pagina_ativada === "tutoriais.php" ? "ativo" : ""?>">
             <i class="fa-solid fa-gamepad"></i>
@@ -20,4 +25,18 @@ $pagina_ativada = $pagina_ativada ?? "";
             <p>Cartas</p>
         </a>
     </nav>
+
+    <div id="acoes-header">
+        <a href="login.php">
+            <i class="fa-solid fa-user"></i>
+        </a>
+
+        <a id="carrinho" href="carrinho.php">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </a>
+
+        <button id="menu-mobile" type="button">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+    </div>
 </header>
