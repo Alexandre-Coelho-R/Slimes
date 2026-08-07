22565e5cbd1d0780eb12c0940544bc131e02d27e
Luca Bressan Moretti <luca.bressan@unesp.br>
2026-08-06 21:37:25 -0300

    edit/add/tudo/ pagina inicial refeita e mudanças na responsividade /06/08

--- index.php
+++ index.php
@@ -11,44 +11,70 @@
     $pagina_ativada = "index.php";
     include "assets/componentes/header.php";
     ?>
-
-    <main>
-        <section id="hero-section">
-            <div id="textos">
-                <h1>Pocket Slimes</h1>
-                <h2>O melhor (e único) TCG de slimes do mundo!</h2>
-            </div>
-            <img class="um" src="assets/imagens/cartas.webp" alt="Imagem contendo algumas das cartas do jogo">
-            <img class="dois" src="assets/imagens/cartas2.webp" alt="Imagem contendo algumas das cartas do jogo">
-        </section>
-
-
-        <section class="site-navigation">
-            <i class="fa-solid fa-book-open"></i>
-            <div class="secundario-textos">
-                <h1 class="sem-emoji">Apresentação do projeto</h1>
-                <h1 class="com-emoji"><i class="fa-solid fa-book-open"></i> Apresentação do projeto</h1>
-                <p>Pocket Slimes é um jogo de cartas desenvolvido por alunos do 2º ano do Colégio Técnico Industrial (CTI) "Prof. Isaac Portal Roldán" de Bauru como parte de um projeto do curso de informática. O jogo tem todos os desenhos feitos à mão sem uso de IA e combina estratégia, criatividade e diversão em partidas protagonizadas por diferentes tipos de slimes, itens, ações e ferramentas.</p>
-            </div>
-        </section>
-       
-        <section class="site-navigation">
-            <i class="fa-solid fa-calendar-days"></i>
-            <div class="secundario-textos">
-                <h1 class="sem-emoji">Como comprar o jogo?</h1>
-                <h1 class="com-emoji"><i class="fa-solid fa-calendar-days"></i> Como comprar o jogo?</h1>
-                <p>O jogo será vendido presencialmente, principalmente durante a Semana do Colégio CTI.</p>
-            </div>
-        </section>
+    <section id="hero-section">
+        <img src="assets/imagens/Banner.png" alt="Banner">
         
-        <section class="site-navigation">
-            <i class="fa-solid fa-envelope"></i>
-            <div class="secundario-textos">
-                <h1 class="sem-emoji">Equipe e contato</h1>
-                <h1 class="com-emoji"><i class="fa-solid fa-envelope"></i> Equipe e contato</h1>
-                <p>O projeto é desenvolvido pelos alunos Renan Coelho, Luca Bressan, Frederico Eckhardt e Kauã César. Para conhecer a equipe, tirar dúvidas ou obter mais informações, entre em contato pelo e-mail pocket.slimes.cti@gmail.com.</p>
-            </div>
-        </section>
+        <div class="title-container">
+            <h1 class="title-outline">SLIME SMASH</h1>
+            <h1 class="title-fill">SLIME SMASH</h1>
+            
+        </div>
+
+        <div class="title-container">
+            <h2 class="title-fill">O melhor jogo TCG do CTI</h2>
+            <h2 class="title-outline">O melhor jogo TCG do CTI</h2>
+        </div>
+
+
+        <div id="hero-buttons">
+            <a href="#first-section">Conheça mais</a>
+            <a href="carrinho.php">Comprar agora</a>
+        </div>
+
+    </section>
+    <main>
+        <nav id="other-pages">
+
+            <a class="site-navigation" id="first-section" href="tutoriais.php">
+                <div class="site-navigation-img">
+                    <img src="assets/imagens/Banner.png" alt="Banner">
+                </div>
+                <div class="site-navigation-text">
+                    <h2>Como Jogar?</h2>
+                    <p>Nesse artigo, você aprenderá tudo o que precisa saber para jogar Slime Smash! Desde as regras básicas até conceitos mais avançados. Seja mais um jogador e vença contra seus slimes rivais!</p>
+                </div>
+            </a>
+
+            <a class="site-navigation" href="f-mvv.php">
+                <div class="site-navigation-img">
+                    <img src="assets/imagens/Banner.png" alt="Banner">
+                </div>
+                <div class="site-navigation-text">
+                    <h2>Conheça nossos conteúdos</h2>
+                    <p>Nesse artigo, você será apresentado às diversas formas as quais estamos presentes na internet, principalmente redes socias. Venha ver nossos vídeos de curiosidades sobre o jogo e algumas ideias de decks para você testar!</p>
+                </div>
+            </a>
+        
+            <a class="site-navigation" href="faq.php">
+                <div class="site-navigation-img">
+                    <img src="assets/imagens/Banner.png" alt="Banner">
+                </div>
+                <div class="site-navigation-text">
+                    <h2>Como comprar e retirar seus produtos?</h2>
+                    <p>Nesse artigo você aprenderá como funciona a compra dos produtos de Slime Smash, tanto dos boosters quanto dos decks prontos. Você verá como comprá-los no site, onde e quando retirá-los PRESENCIALMENTE. Não tenha mais dúvidas!</p>
+                </div>
+            </a>
+
+            <a class="site-navigation" href="carrinho.php">
+                <div class="site-navigation-img">
+                    <img src="assets/imagens/Banner.png" alt="Banner">
+                </div>
+                <div class="site-navigation-text">
+                    <h2>Faça suas compras!</h2>
+                    <p>Nesse local, será apresentado uma lista de todos os produtos de Slime Smash para você comprar presencialmente, ou reservar para retirada.</p>
+                </div>
+            </a>
+        </nav>
     </main>
 
     <?php include "assets/componentes/footer.php"?>
