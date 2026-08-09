ff8e3004af1754016a3eda21dc5534d039905ce1
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-09 13:50:29 -0300

    Ajustes principalmente na Homepage

--- index.php
+++ index.php
@@ -5,19 +5,10 @@ include "assets/componentes/head-header.php";
 ?>
 
 <section id="hero-section">
-    <img src="assets/imagens/Banner.png" alt="Banner">
+    <img src="assets/imagens/banner.png" alt="Banner">
     
-    <div class="title-container">
-        <h1 class="title-outline">SLIME SMASH</h1>
-        <h1 class="title-fill">SLIME SMASH</h1>
-        
-    </div>
-
-    <div class="title-container">
-        <h2 class="title-fill">O melhor jogo TCG do CTI</h2>
-        <h2 class="title-outline">O melhor jogo TCG do CTI</h2>
-    </div>
-
+    <h1 class="hero-title">SLIME SMASH</h1>
+    <h2 class="hero-subtitle">O melhor jogo TCG do CTI</h2>
 
     <div id="hero-buttons">
         <a href="#first-section">
@@ -36,9 +27,7 @@ include "assets/componentes/head-header.php";
     <nav id="other-pages">
 
         <a class="site-navigation" id="first-section" href="tutoriais.php">
-            <div class="site-navigation-img">
-                <img src="assets/imagens/Banner.png" alt="Banner">
-            </div>
+            <img src="assets/imagens/banner.png" alt="Banner">
             <div class="site-navigation-text">
                 <h2>Como Jogar?</h2>
                 <p>Nesse artigo, você aprenderá tudo o que precisa saber para jogar Slime Smash! Desde as regras básicas até conceitos mais avançados. Seja mais um jogador e vença contra seus slimes rivais!</p>
@@ -46,9 +35,7 @@ include "assets/componentes/head-header.php";
         </a>
 
         <a class="site-navigation" href="f-mvv.php">
-            <div class="site-navigation-img">
-                <img src="assets/imagens/Banner.png" alt="Banner">
-            </div>
+            <img src="assets/imagens/banner.png" alt="Banner">
             <div class="site-navigation-text">
                 <h2>Conheça a gente mais a fundo</h2>
                 <p>Nesse artigo, você entenderá um pouco mais sobre a nossa empresa, nossas missões, visões e valores, além dos nossos conteúdos nas redes sociais</p>
@@ -56,9 +43,7 @@ include "assets/componentes/head-header.php";
         </a>
     
         <a class="site-navigation" href="faq.php">
-            <div class="site-navigation-img">
-                <img src="assets/imagens/Banner.png" alt="Banner">
-            </div>
+            <img src="assets/imagens/banner.png" alt="Banner">
             <div class="site-navigation-text">
                 <h2>Como comprar e retirar seus produtos?</h2>
                 <p>Nesse artigo você aprenderá como funciona a compra dos produtos de Slime Smash. Você verá como comprá-los no site, onde e quando retirá-los. Não tenha mais dúvidas!</p>
@@ -66,9 +51,7 @@ include "assets/componentes/head-header.php";
         </a>
 
         <a class="site-navigation" href="carrinho.php">
-            <div class="site-navigation-img">
-                <img src="assets/imagens/Banner.png" alt="Banner">
-            </div>
+            <img src="assets/imagens/banner.png" alt="Banner">
             <div class="site-navigation-text">
                 <h2>Faça suas compras!</h2>
                 <p>Nesse local, será apresentado uma lista de todos os produtos de Slime Smash para você comprar presencialmente, ou reservar para retirada.</p>
