46c1c67d684f62bfbf59e5e39a921378e2fe7011
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-10 19:56:39 -0300

    Fiz a página desenvolvedores

--- f-desenvolvedores.php
+++ f-desenvolvedores.php
@@ -10,62 +10,61 @@ include "assets/componentes/head-header.php";
 
     <section id="devs">
         <div>
-            <img src="assets/imagens/membros/frederico.webp" alt="Foto do membro Frederico">
-            <div>
-                <h2>Frederico Eckhardt</h2>
-                <h2>N° 07</h2>
-                <p><b>Produções</b></p>
+            <div class="devs-imagem">
+                <img src="assets/imagens/membros/frederico.webp" alt="Foto do membro Frederico">
+            </div>
+            <div class="devs-texto">
+                <h2>Frederico Eckhardt nº 07</h2>
                 <ul>
                     <li>Artista plástico;</li>
                     <li>Produção das moedas comemorativas;</li>
-                    <li>Planejamento e confecção do stand de vendas;</li>
+                    <li>Planejamento e confecção do stand de vendas.</li>
                 </ul>
             </div>
         </div>
 
         <div>
-            <img src="assets/imagens/membros/kaua.webp" alt="Foto do membro Kauã">
-            <div>
-                <h2>Kauã César da Silva Bento</h2>
-                <h2>N° 18</h2>
-
-                <p><b>Produções</b></p>
+            <div class="devs-imagem">
+                <img src="assets/imagens/membros/kaua.webp" alt="Foto do membro Kauã">
+            </div>
+            <div class="devs-texto">
+                <h2>Kauã César da Silva Bento nº 18</h2>
                 <ul>
                     <li>Desenvolvedor das mecânicas de jogo;</li>
                     <li>Balanceamento e confecção dos decks;</li>
                     <li>Beta tester;</li>
-                    <li>Confecção do stand de vendas;</li>
+                    <li>Confecção do stand de vendas.</li>
                 </ul>
             </div>
         </div>
 
         <div>
-            <img src="assets/imagens/membros/luca.webp" alt="Foto do membro Luca">
-            <div>
-                <h2>Luca Bressan Moretti</h2>
-                <h2>N° 22</h2>
-                <p><b>Produções</b></p>
+            <div class="devs-imagem">
+                <img src="assets/imagens/membros/luca.webp" alt="Foto do membro Luca">
+            </div>
+            <div class="devs-texto">
+                <h2>Luca Bressan Moretti nº 22</h2>
                 <ul>
                     <li>Iniciativa do projeto e líder do grupo;</li>
                     <li>Desenvolvedor front-end;</li>
                     <li>Artista gráfico, confecção das cartas e artes para o jogo e à empresa;</li>
                     <li>Beta tester;</li>
-                    <li>Criador do conceito visual do jogo;</li>
+                    <li>Criador do conceito visual do jogo.</li>
                 </ul>
             </div>
         </div>
 
         <div>
-            <img src="assets/imagens/membros/renan.webp" alt="Foto do membro Renan">
-            <div>
-                <h2>Renan Alexandre Coelho</h2>
-                <h2>N° 28</h2>
-                <p><b>Produções</b></p>
+            <div class="devs-imagem">
+                <img src="assets/imagens/membros/renan.webp" alt="Foto do membro Renan">
+            </div>
+            <div class="devs-texto">
+                <h2>Renan Alexandre Coelho nº 28</h2>
                 <ul>
                     <li>Desenvolvedor back-end;</li>
                     <li>Integração do website com banco de dados;</li>
                     <li>Confecção da estrutura do website;</li>
-                    <li>Desenvolvedor da aplicação em C# do caixa;</li>
+                    <li>Desenvolvedor da aplicação em C# do caixa.</li>
                 </ul>
             </div>
         </div>
