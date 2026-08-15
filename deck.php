73a737d1ccd8b8b23e4b67ed65f1efa5e9103a2f
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-15 16:38:27 -0300

    Peguei as mudanças de kauã

--- deck.php
+++ deck.php
@@ -1,74 +1,120 @@
-<?php 
-$titulo = "Deck";
-$css = "deck.css";
-$js = "produtos.js";
-
-include "assets/componentes/head-header.php";
-include "assets/funcoes/lista-deck.php";
-
-$deck = $_GET['deck'] ?? '';
-
-if (!isset($decks[$deck])) {
-    die("Deck não encontrado.");
-}
-
-$produto = $decks[$deck];
-?>
-
-<main class="pagina-deck">
-
-    <section class="cabecalho-deck">
-
-        <div>
-            <h1><?= $produto['nome'] ?></h1>
-        </div>
-
-        <div class="valor-deck">
-            <span>Valor do Deck</span>
+<?php  
+$titulo = "Deck"; 
+$css = "deck.css"; 
+$js = "produtos.js"; 
+ 
+include "assets/componentes/head-header.php"; 
+include "assets/funcoes/lista-deck.php"; 
+ 
+$deck = $_GET['deck'] ?? ''; 
+ 
+if (!isset($decks[$deck])) { 
+    die("Deck não encontrado."); 
+} 
+ 
+$produto = $decks[$deck]; 
+?> 
+ 
+<main class="pagina-deck"> 
+ 
+    <section class="cabecalho-deck"> 
+ 
+        <div> 
+            <h1><?= $produto['nome'] ?></h1> 
+        </div> 
+ 
+        <div class="valor-deck"> 
+            <span>Valor do Deck</span> 
             <strong><?= $produto['preco'] ?></strong>
-        </div>
-
-    </section>
 
 
-    <section class="conteudo-deck">
+            <!-- ADICIONAR AO CARRINHO -->
 
-        <div class="lista-cartas">
-
-            <?php foreach ($produto['cartas'] as $carta): ?>
-
-                <div 
-                    class="linha-carta"
-                    data-imagem="assets/imagens/cartas/<?= $carta['imagem'] ?>"
-                >
-
-                    <span class="qtd">
-                        <?= $carta['qtd'] ?>
-                    </span>
-
-                    <span class="nome-carta">
-                        <?= $carta['nome'] ?>
-                    </span>
-
-                </div>
-
-            <?php endforeach; ?>
-
-        </div>
-
-
-        <div class="carta-preview">
-
-            <img 
-                id="carta-destaque"
-                src="assets/imagens/cartas/<?= $produto['cartas'][0]['imagem'] ?>"
-                alt="Carta selecionada"
+            <form
+                action="assets/funcoes/carrin.php"
+                method="POST"
+                class="form-carrinho"
             >
 
-        </div>
+                <input
+                    type="hidden"
+                    name="acao"
+                    value="adicionar"
+                >
 
-    </section>
+                <input
+                    type="hidden"
+                    name="id"
+                    value="<?= htmlspecialchars($deck) ?>"
+                >
 
-</main>
+                <input
+                    type="hidden"
+                    name="nome"
+                    value="<?= htmlspecialchars($produto['nome']) ?>"
+                >
 
-<?php include "assets/componentes/footer.php"?>
+                <input
+                    type="hidden"
+                    name="preco"
+                    value="<?= htmlspecialchars($produto['preco']) ?>"
+                >
+
+                <input
+                    type="hidden"
+                    name="imagem"
+                    value="assets/imagens/cartas/<?= htmlspecialchars($produto['cartas'][0]['imagem']) ?>"
+                >
+
+                <button type="submit">
+                    Adicionar
+                </button>
+
+            </form>
+
+        </div> 
+ 
+    </section> 
+ 
+ 
+    <section class="conteudo-deck"> 
+ 
+        <div class="lista-cartas"> 
+ 
+            <?php foreach ($produto['cartas'] as $carta): ?> 
+ 
+                <div  
+                    class="linha-carta" 
+                    data-imagem="assets/imagens/cartas/<?= $carta['imagem'] ?>" 
+                > 
+ 
+                    <span class="qtd"> 
+                        <?= $carta['qtd'] ?> 
+                    </span> 
+ 
+                    <span class="nome-carta"> 
+                        <?= $carta['nome'] ?> 
+                    </span> 
+ 
+                </div> 
+ 
+            <?php endforeach; ?> 
+ 
+        </div> 
+ 
+ 
+        <div class="carta-preview"> 
+ 
+            <img  
+                id="carta-destaque" 
+                src="assets/imagens/cartas/<?= $produto['cartas'][0]['imagem'] ?>" 
+                alt="Carta selecionada" 
+            > 
+ 
+        </div> 
+ 
+    </section> 
+ 
+</main> 
+ 
+<?php include "assets/componentes/footer.php" ?>
--- produtos.php
+++ produtos.php
@@ -1,57 +1,127 @@
-<?php 
-$titulo = "Produtos";
-$css = "produto.css";
-$js = "produtos.js";
-include "assets/componentes/head-header.php";
-?>
+<?php  
+$titulo = "Produtos"; 
+$css = "produto.css"; 
+$js = "produtos.js"; 
 
-<main class="pagina-produtos">
+include "assets/componentes/head-header.php"; 
+?> 
+ 
+<main class="pagina-produtos"> 
+ 
+    <!-- BANNER --> 
+    <div class="banner-produtos"> 
+        <img  
+            src="assets/imagens/banner.webp"  
+            alt="Banner da loja" 
+        > 
+    </div> 
+ 
+    <section class="categoria"> 
+ 
+        <h2 style="text-align: center;">Produtos</h2> 
+ 
+        <div class="carrossel"> 
+ 
+            <div class="produtos"> 
+ 
+                <!-- PRODUTO 1 -->
 
-    <!-- BANNER -->
-    <div class="banner-produtos">
-        <img 
-            src="assets/imagens/banner.webp" 
-            alt="Banner da loja"
-        >
-    </div>
+                <div class="produto">
 
-<section class="categoria">
+                    <a href="deck.php?deck=anciao">
 
-    <h2 style="text-align: center;">Anciao fulcral</h2>
+                        <div class="imagem-produto"> 
+                            <img src="assets/imagens/cartas/ss_acaixa.webp"> 
+                            <span class="quantidade">
+                                42<br><small>unid</small>
+                            </span> 
+                        </div>
 
-    <div class="carrossel">
+                    </a>
 
-        <button class="seta seta-esquerda"></button>
+                    <p class="nome">67</p> 
+                    <strong>R$ 67,00</strong>
 
-        <div class="produtos">
+                    <form
+                        action="assets/funcoes/carrin.php"
+                        method="POST"
+                        class="form-carrinho"
+                    >
 
-            <a href="deck.php?deck=anciao" class="produto">
-                <div class="imagem-produto">
-                    <img src="assets/imagens/cartas/ss_acaixa.webp">
-                    <span class="quantidade">42<br><small>unid</small></span>
+                        <input type="hidden" name="acao" value="adicionar">
+
+                        <input type="hidden" name="id" value="anciao">
+
+                        <input type="hidden" name="nome" value="67">
+
+                        <input type="hidden" name="preco" value="67">
+
+                        <input
+                            type="hidden"
+                            name="imagem"
+                            value="assets/imagens/cartas/ss_acaixa.webp"
+                        >
+
+                        <button type="submit">
+                            Adicionar
+                        </button>
+
+                    </form>
+
                 </div>
 
-                <p class="nome">67</p>
-                <strong>R$ 67,00</strong>
-            </a>
 
+                <!-- PRODUTO 2 -->
 
-            <a href="deck.php?deck=outro-deck" class="produto">
-                <div class="imagem-produto">
-                    <img src="assets/imagens/cartas/ss_slimeferreiro.webp">
-                    <span class="quantidade">67<br><small>unid</small></span>
+                <div class="produto">
+
+                    <a href="deck.php?deck=outro-deck">
+
+                        <div class="imagem-produto"> 
+                            <img src="assets/imagens/cartas/ss_slimeferreiro.webp"> 
+                            <span class="quantidade">
+                                67<br><small>unid</small>
+                            </span> 
+                        </div>
+
+                    </a>
+
+                    <p class="nome">42</p> 
+                    <strong>R$ 42,00</strong>
+
+                    <form
+                        action="assets/funcoes/carrin.php"
+                        method="POST"
+                        class="form-carrinho"
+                    >
+
+                        <input type="hidden" name="acao" value="adicionar">
+
+                        <input type="hidden" name="id" value="outro-deck">
+
+                        <input type="hidden" name="nome" value="42">
+
+                        <input type="hidden" name="preco" value="42">
+
+                        <input
+                            type="hidden"
+                            name="imagem"
+                            value="assets/imagens/cartas/ss_slimeferreiro.webp"
+                        >
+
+                        <button type="submit">
+                            Adicionar
+                        </button>
+
+                    </form>
+
                 </div>
 
-                <p class="nome">42</p>
-                <strong>R$ 42,00</strong>
-            </a>
+            </div> 
+ 
+        </div> 
+    </section> 
 
-        </div>
-
-        <button class="seta seta-direita">›</button>
-
-    </div>
-</section>
-</main>
-
+</main> 
+ 
 <?php include "assets/componentes/footer.php"; ?>
