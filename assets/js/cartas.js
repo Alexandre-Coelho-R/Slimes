178d1481ffea99daf15c94474bbf20bf3d5c0d4b
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-15 16:18:52 -0300

    A lista de cartas foi para o sql

--- assets/js/cartas.js
+++ assets/js/cartas.js
@@ -1,233 +1,12 @@
 //Inicializar variáveis
 
 const catalogo = document.getElementById("catalogo");
-
 const btTodos = document.getElementById("todos");
 const btSlimes = document.getElementById("slimes");
 const btItens = document.getElementById("itens");
 const btAcoes = document.getElementById("acoes");
 const btFerramentas = document.getElementById("ferramentas");
-
 const cartaAmpliada = document.getElementById("carta-ampliada");
-
-const cartas = [
-  {
-    nome: "Atomislime",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_atomislime.webp"
-  },
-  {
-    nome: "DJ Slime",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_djslime.webp"
-  },
-  {
-    nome: "Draconislime",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_draconislime.webp"
-  },
-  {
-    nome: "Slime alquimista",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimealquimista.webp"
-  },
-  {
-    nome: "Slime aquático",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeaquatico.webp"
-  },
-  {
-    nome: "Slime buraco negro",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeburaconegro.webp"
-  },
-  {
-    nome: "Slime clérigo",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeclerigo.webp"
-  },
-  {
-    nome: "Slime comum",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimecomum.webp"
-  },
-  {
-    nome: "Slime cozinheiro",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimecozinheiro.webp"
-  },
-  {
-    nome: "Slime de cola",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimedecola.webp"
-  },
-  {
-    nome: "Slime de grama",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimedegrama.webp"
-  },
-  {
-    nome: "Slime de magma",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimedemagma.webp"
-  },
-  {
-    nome: "Slime elétrico",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeeletrico.webp"
-  },
-  {
-    nome: "Slime enfermeiro",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeenfermeiro.webp"
-  },
-  {
-    nome: "Slime escavador",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeescavador.webp"
-  },
-  {
-    nome: "Slime espadachim",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeespadachim.webp"
-  },
-  {
-    nome: "Slime ferreiro",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeferreiro.webp"
-  },
-  {
-    nome: "Slime gigantesco",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimegigantesco.webp"
-  },
-  {
-    nome: "Slime mágico",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimemagico.webp"
-  },
-  {
-    nome: "Slime mago",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimemago.webp"
-  },
-  {
-    nome: "Slime necromante",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimenecromante.webp"
-  },
-  {
-    nome: "Slime pescador",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimepescador.webp"
-  },
-  {
-    nome: "Slime pistoleiro",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimepistoleiro.webp"
-  },
-  {
-    nome: "Slime primitivo",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeprimitivo.webp"
-  },
-  {
-    nome: "Slime solar",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimesolar.webp"
-  },
-  {
-    nome: "Slime viking",
-    categoria: "slime",
-    imagem: "assets/imagens/cartas/ss_slimeviking.webp"
-  },
-  {
-    nome: "A caixa",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_acaixa.webp"
-  },
-  {
-    nome: "Soprador de slime",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_sopradordeslime.webp"
-  },
-  {
-    nome: "Aspirador de slime",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_aspiradordeslimes.webp"
-  },
-  {
-    nome: "Árvore de slime",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_arvoredegosma.webp"
-  },
-  {
-    nome: "Kit caça slime",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_kitcacaslime.webp"
-  },
-  {
-    nome: "Mapa de gosmas",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_mapadegosmas.webp"
-  },
-  {
-    nome: "Poção de cura",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_pocaodecura.webp"
-  },
-  {
-    nome: "Slimonomicon",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_slimonomicon.webp"
-  },
-  {
-    nome: "Impressora",
-    categoria: "item",
-    imagem: "assets/imagens/cartas/ss_impressora.webp"
-  },
-  {
-    nome: "Apostar tudo",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_apostartudo.webp"
-  },
-  {
-    nome: "Dança das cadeiras",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_dancadascadeiras.webp"
-  },
-  {
-    nome: "Eu escolho você",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_euescolhovoce.webp"
-  },
-  {
-    nome: "Imitar",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_imitar.webp"
-  },
-  {
-    nome: "Limpar",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_limpar.webp"
-  },
-  {
-    nome: "Pesquisar",
-    categoria: "ação",
-    imagem: "assets/imagens/cartas/ss_pesquisar.webp"
-  },
-  {
-    nome: "Relação interespecífica",
-    categoria: "ferramenta",
-    imagem: "assets/imagens/cartas/ss_relacaointerespecie.webp"
-  },
-  {
-    nome: "Ritual de desespero",
-    categoria: "ferramenta",
-    imagem: "assets/imagens/cartas/ss_ritualdedesespero.webp"
-  }
-];
-
 const possuiMouse = window.matchMedia("(pointer: fine)").matches;
 
 //Função de mostrar cartas
@@ -236,7 +15,7 @@ function mostrarCartas(lista){
     catalogo.innerHTML = "";
     lista.forEach((carta) => {
         const imagem = document.createElement("img");
-        imagem.src = carta.imagem;
+        imagem.src = "assets/imagens/cartas/" + carta.imagem + ".webp";
         imagem.alt = carta.nome;
 
         imagem.addEventListener("click", () => {
@@ -266,7 +45,7 @@ function ampliarCarta(carta){
     cartaAmpliada.innerHTML = "";
 
     const cartaCriada = document.createElement("img");
-    cartaCriada.src = carta.imagem;
+    cartaCriada.src = "assets/imagens/cartas/" + carta.imagem + ".webp";
     cartaCriada.alt = carta.nome;
 
     cartaAmpliada.appendChild(cartaCriada);
@@ -306,9 +85,19 @@ function ampliarCarta(carta){
     }
 }
 
+// Função de pegar as cartas do banco de dados
+
+let cartas = [];
+
+async function carregarCartas() {
+	const resultado = await fetch("assets/funcoes/buscar-cartas.php");
+	cartas = await resultado.json();
+	mostrarCartas(cartas);
+}
+
 //Inicialização
 
-mostrarCartas(cartas);
+carregarCartas();
 btTodos.style.backgroundColor = "lightgreen";
 
 cartaAmpliada.addEventListener("click", (evento) => {
