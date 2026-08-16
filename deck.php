abd5ed6ee5436b4a719e082db63ed628429f9d81
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-08-15 22:11:50 -0300

    Sensação de poder absurda

--- deck.php
+++ deck.php
@@ -31,32 +31,40 @@ if (!$produto) voltarInfo("Produto não encontrado.");
 
             <form action="assets/funcoes/carrin.php" method="POST" class="form-carrinho">
                 <input type="hidden" name="acao "value="adicionar">
-                <input type="hidden" name="id "value=<?=$_GET["id"]?>>
+                <input type="hidden" name="id_produto"value=<?=$_GET["id"]?>>
                 <button type="submit">Adicionar</button>
             </form>
         </div> 
     </section> 
 
     <?php
-    $sql = "SELECT * FROM deck_carta WHERE fk_produto=:id_produto";
+    $sql = "
+    SELECT carta.nome, carta.imagem, deck_carta.quantidade
+    FROM deck_carta
+    INNER JOIN carta
+    ON carta.id_carta = deck_carta.fk_carta
+    WHERE deck_carta.fk_produto = :id_produto
+    ";
     $select = $conn -> prepare($sql);
     $select -> execute([":id_produto" => $_GET["id"]]);
-    $cartas = $select -> fetch(PDO::FETCH_ASSOC);
+    $cartas = $select -> fetchAll(PDO::FETCH_ASSOC);
     ?>
 
     <section class="conteudo-deck"> 
         <div class="lista-cartas"> 
-            <?php foreach ($produto['cartas'] as $carta): ?> 
-                <div class="linha-carta" data-imagem="assets/imagens/cartas/<?= $carta['imagem'] ?>" > 
-                    <span class="qtd"><?= $carta['qtd'] ?></span> 
-                    <span class="nome-carta"><?= $carta['nome'] ?></span> 
+            <?php foreach ($cartas as $carta): ?> 
+                <div class="linha-carta" data-imagem="assets/imagens/cartas/<?=$carta['imagem']?>.webp" > 
+                    <span class="qtd"><?=$carta['quantidade'] ?></span> 
+                    <span class="nome-carta"><?=$carta['nome'] ?></span> 
                 </div> 
             <?php endforeach; ?> 
         </div> 
  
-        <div class="carta-preview"> 
-            <img id="carta-destaque" src="assets/imagens/cartas/<?= $produto['cartas'][0]['imagem'] ?>" alt="Carta selecionada"> 
-        </div> 
+        <?php if (!empty($cartas)): ?>
+            <div class="carta-preview">
+                <img id="carta-destaque" src="assets/imagens/cartas/<?=$cartas[0]['imagem']?>.webp" alt="<?= htmlspecialchars($cartas[0]['nome']) ?>">
+            </div>
+        <?php endif;?>
     </section> 
 
 </main> 
