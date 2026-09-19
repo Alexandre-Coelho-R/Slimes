0bf6f8d956472f20c3df89d64d5e838d27862ef0
Alexandre-Coelho-R <alexandre.coelho@unesp.br>
2026-09-19 19:26:56 -0300

    Links das redes sociais atualizados

--- _rodape.php
+++ _rodape.php
@@ -8,16 +8,16 @@
         </div>
 
         <div id="redes_contato">
-            <a href="https://www.instagram.com/pocket.slimes.cti/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/slime_smash_cti/" target="_blank" rel="noopener noreferrer">
                 <img src="assets/imagens/redes-sociais/instagram.webp" alt="Instagram do Slime Smash">
             </a>
-            <a href="https://www.youtube.com/@PocketSlimes" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.youtube.com/@slime_smash_cti" target="_blank" rel="noopener noreferrer">
                 <img src="assets/imagens/redes-sociais/youtube.webp" alt="Youtube do Slime Smash" id="youtube">
             </a>
-            <a href="https://www.facebook.com/profile.php?id=61590291854536" target="_blank" rel="noopener noreferrer">
+            <a href="https://web.facebook.com/profile.php?id=61590291854536" target="_blank" rel="noopener noreferrer">
                 <img src="assets/imagens/redes-sociais/facebook.webp" alt="Facebook do Slime Smash">
             </a>
-            <a href="https://www.tiktok.com/@pocket.slimes.cti" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.tiktok.com/@slime_smash_cti" target="_blank" rel="noopener noreferrer">
                 <img src="assets/imagens/redes-sociais/tiktok.webp" alt="Tiktok do Slime Smash">
             </a>
         </div>
